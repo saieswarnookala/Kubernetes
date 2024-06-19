09f348d55aef98f508f9178bdc500e3d243b1310
Sai Eswar Nookala <108252839+saieswarnookala@users.noreply.github.com>
2024-06-18 23:39:24 -0400

    Update myshell.sh

--- myshell.sh
+++ myshell.sh
@@ -47,7 +47,7 @@ curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sleep 60
-sudo apt-get install -y kubelet='1.29.0-1.1' kubectl='1.29.0-1.1' kubectl='1.29.0-1.1'
+sudo apt-get install -y kubelet='1.29.0.1-1' kubectl='1.29.0.1-1' kubectl='1.29.0.1-1'
 sleep 100
 sudo apt-mark hold kubelet kubeadm kubectl
 #(Optional) Enable the kubelet service before running kubeadm:
