083ce180fbf5dbe65df18bb6c8ca38eccdf80e43
Sai Eswar Nookala <108252839+saieswarnookala@users.noreply.github.com>
2024-06-19 16:09:25 -0400

    Update prometheus.sh

--- prometheus.sh
+++ prometheus.sh
@@ -2,8 +2,7 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 helm repo update
 helm install prometheus prometheus-community/prometheus
 kubectl expose service prometheus-server --type=NodePort --target-port=9090 --name=prometheus-server-ext
-echo "
-apiVersion: v1
+echo "apiVersion: v1
 kind: PersistentVolume
 metadata:
   name: prometheus-server-pv
